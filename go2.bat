8d23ed7fae76d6cab293830ca6cd477938e858b0
raunaqjabbal <raunaq.jabbal012@gmail.com>
2024-05-05 12:56:39 +0530

    multiobjective scfipt

---
+++ go2.bat
@@ -0,0 +1,52 @@
+@echo off
+set repetitions=15
+set savepath= exploration_ignore
+
+@REM START cmd /k python run.py -a age -o maximin speed --r %repetitions% -p %savepath%
+@REM START cmd /k python run.py -a gender -o maximin speed --r %repetitions% -p %savepath%
+@REM START cmd /k python run.py -a ethnicity -o maximin speed --r %repetitions% -p %savepath%
+
+@REM START cmd /k python run.py -a age -o maximin speed --r %repetitions% -p %savepath%
+@REM START cmd /k python run.py -a gender -o maximin speed --r %repetitions% -p %savepath%
+@REM START cmd /k python run.py -a ethnicity -o maximin speed --r %repetitions% -p %savepath%
+
+@REM START cmd /k python run.py -a age -o maximin speed --r %repetitions% -p %savepath%
+@REM START cmd /k python run.py -a gender -o maximin speed --r %repetitions% -p %savepath%
+@REM START cmd /k python run.py -a ethnicity -o maximin speed --r %repetitions% -p %savepath%
+
+@REM START cmd /k python run.py -a age -o maximin rationality --r %repetitions% -p %savepath%
+@REM START cmd /k python run.py -a gender -o maximin rationality --r %repetitions% -p %savepath%
+@REM START cmd /k python run.py -a ethnicity -o maximin rationality --r %repetitions% -p %savepath%
+
+@REM START cmd /k python run.py -a age -o maximin rationality --r %repetitions% -p %savepath%
+@REM START cmd /k python run.py -a gender -o maximin rationality --r %repetitions% -p %savepath%
+@REM START cmd /k python run.py -a ethnicity -o maximin rationality --r %repetitions% -p %savepath%
+
+@REM START cmd /k python run.py -a age -o maximin rationality --r %repetitions% -p %savepath%
+@REM START cmd /k python run.py -a gender -o maximin rationality --r %repetitions% -p %savepath%
+@REM START cmd /k python run.py -a ethnicity -o maximin rationality --r %repetitions% -p %savepath%
+
+@REM START cmd /k python run.py -a age -o speed rationality --r %repetitions% -p %savepath%
+@REM START cmd /k python run.py -a gender -o speed rationality --r %repetitions% -p %savepath%
+@REM START cmd /k python run.py -a ethnicity -o speed rationality --r %repetitions% -p %savepath%
+
+@REM START cmd /k python run.py -a age -o speed rationality --r %repetitions% -p %savepath%
+@REM START cmd /k python run.py -a gender -o speed rationality --r %repetitions% -p %savepath%
+@REM START cmd /k python run.py -a ethnicity -o speed rationality --r %repetitions% -p %savepath%
+
+@REM START cmd /k python run.py -a age -o speed rationality --r %repetitions% -p %savepath%
+@REM START cmd /k python run.py -a gender -o speed rationality --r %repetitions% -p %savepath%
+@REM START cmd /k python run.py -a ethnicity -o speed rationality --r %repetitions% -p %savepath%
+
+@REM START cmd /k python run.py -a age -o maximin speed rationality --r %repetitions% -p %savepath%
+@REM START cmd /k python run.py -a gender -o maximin speed rationality --r %repetitions% -p %savepath%
+@REM START cmd /k python run.py -a ethnicity -o maximin speed rationality --r %repetitions% -p %savepath%
+
+@REM START cmd /k python run.py -a age -o maximin speed rationality --r %repetitions% -p %savepath%
+@REM START cmd /k python run.py -a gender -o maximin speed rationality --r %repetitions% -p %savepath%
+@REM START cmd /k python run.py -a ethnicity -o maximin speed rationality --r %repetitions% -p %savepath%
+
+@REM START cmd /k python run.py -a age -o maximin speed rationality --r %repetitions% -p %savepath%
+@REM START cmd /k python run.py -a gender -o maximin speed rationality --r %repetitions% -p %savepath%
+@REM START cmd /k python run.py -a ethnicity -o maximin speed rationality --r %repetitions% -p %savepath%
+
